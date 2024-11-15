56d00675c6938134b21e766edb7e60860bab94da
flor <@floatinglife.com>
2024-11-15 19:06:29 +0000

    fix some config error

--- lua/lazy/plugins/conform.lua
+++ lua/lazy/plugins/conform.lua
@@ -21,13 +21,13 @@ return {
 				async = false,
 				timeout_ms = 500,
 			},
-      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-        require("conform").format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 500,
-        })
-      end, { desc = "Format file or range (in visual mode)" }),
+			vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 500,
+				})
+			end, { desc = "Format file or range (in visual mode)" }),
 		})
 	end,
 }
